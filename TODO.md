4bc7010c459258e4c7ecbd514abc9e83b76e41b8
Jose David <Jd>
2023-06-19 20:30:47 +0200

    se hacen los 2 primeros puntos

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 # TODO:
+
     - usuario acepta nombre y/o correo en vez/además de generar
     - Se pueden crear almunos con nombre y/o correo
     - Profesores heredan de User (por tanto, heredan funcionalidad)
--- user.py
+++ user.py
@@ -1,10 +1,11 @@
 from generarNombre import GenerarNombre
 
 class User:
-    def __init__(self):
+
+    def __init__(self, nombre, correo):
         gen = GenerarNombre()
         self.nombre = gen.get_nombre()
         self.correo = gen.get_correo()
+        
 
-        def __str__(self):
-            return f"{self.nombre} - {self.correo}"
+  
